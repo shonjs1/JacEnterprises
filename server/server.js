eadf4ebbd964fa386a29220dc4bbb0ca89adf56f
shonjs <jihyukshon@gmail.com>
2023-09-11 16:05:26 -0400

    added CORS for frontend/backend communication, created a basic API to test fetch

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const names =['James', 'John', 'Jeff', 'Jim']
+//const cors = require('cors');
 
-app.get("/api", (req,res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+//app.use(cors());
+
+app.get("/users", (req,res) => {
+    res.json(names)
 })
 
 app.listen(5000, () => {console.log("Server started on port 5000")})
+
