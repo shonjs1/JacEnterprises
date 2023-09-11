e2f0f6ff83c932e9f227799d9067c027bc40b23a
shonjs <jihyukshon@gmail.com>
2023-09-11 16:01:05 -0400

    Able to successful link frontend + backend with proxy with List of Name Example

--- client/vite-project/src/App.tsx
+++ client/vite-project/src/App.tsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [names, setNames] = useState(['Huan', 'Cameron', 'Alicia', 'Jay'])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  function namesAsList() {
+    const allLists = names.map (n => <li key={n}>{n}</li>)
+    return allLists
+  }
+
+  function handleClick() {
+    fetch('http://localhost:5000/users')
+    .then(res =>  {
+      return res.json()
+    }).then(val => {
+      setNames(val)
+    })
+  }
+
+  return(
+    <div className="App">
+      <h1>List of names</h1>
+      <ul>
+        {namesAsList()}
+      </ul>
+      <button onClick={handleClick}>Update Names</button>
+    </div>
   )
 }
 
