3aa516411ae9e20b51c477561fa6a481a9d8846c
shonjs <jihyukshon@gmail.com>
2023-09-12 20:45:21 -0400

    Added files to support data transfer from backend to frontend

--- server/server.js
+++ server/server.js
@@ -1,13 +1,51 @@
-const express = require('express')
-const app = express()
-const names =['James', 'John', 'Jeff', 'Jim']
-//const cors = require('cors');
+const express = require('express');
+const axios = require('axios'); // Import axios
+const cors = require('cors');
+const app = express();
+const port = 5000;
 
-//app.use(cors());
+app.use(cors()); // Enable CORS for all routes
 
-app.get("/users", (req,res) => {
-    res.json(names)
-})
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+let history = [];
 
+async function getNasa(search_term) {
+  try {
+    const response = await axios.get("https://images-api.nasa.gov/search?q=" + search_term);
+    if (response.status !== 200) {
+      throw new Error('Failed to fetch data from NASA API');
+    }
+
+    const data = response.data.collection.items;
+    
+    if (data.length === 0) {
+      throw new Error('No items found in NASA API response');
+    }
+
+    const randomItem = data[Math.floor(Math.random() * data.length)];
+
+    return randomItem;
+  } catch (error) {
+    console.error('Error fetching data from NASA API:', error);
+    throw error;
+  }
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const image = await getNasa(req.query.q);
+    history.push(image.links[0].href);
+    res.status(200).json({
+      "message": "Object Saved!",
+      "history": history
+    });
+  } catch (error) {
+    res.status(500).json({
+      "message": "Error fetching data from NASA API"
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port:${port}`);
+});
