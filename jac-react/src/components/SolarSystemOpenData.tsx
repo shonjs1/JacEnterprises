e63e285c378bc64cddf35d09fc062f7756af29d7
Numbah6 <89164785+Numbah6@users.noreply.github.com>
2023-09-14 11:23:37 -0400

    got to a point where the api can show something

--- jac-react/src/components/SolarSystemOpenData.tsx
+++ jac-react/src/components/SolarSystemOpenData.tsx
@@ -1,33 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const SolarSystemData: React.FC = () => {
-    const [data, setData] = useState<any[]>([]);
+const SolarSystemOpenData: React.FC = () => {
+    const [solarData, setSolarData] = useState<any | null>(null);
 
     useEffect(() => {
-        fetch('https://api.le-systeme-solaire.net/rest/')
-            .then((response) => response.json())
-            .then((data) => {
-            console.log('API Data:', data); // Log the data
-            setData(data);
-            })
-            .catch((error) => {
-            console.error('Error fetching data:', error);
-            });
-    }, []);
+    fetch("https://api.le-systeme-solaire.net/rest/bodies/")
+        .then((response) => response.json())
+        .then((data) => {
+        const celestialBodies = data.bodies.filter(
+            (body: any) => body.isPlanet && !body.isDwarfPlanet
+        );
 
-return (
+        // Generate a random index to select a celestial body
+        const randomIndex = Math.floor(Math.random() * celestialBodies.length);
+
+        // Set the selected celestial body
+        setSolarData(celestialBodies[randomIndex]);
+        })
+        .catch((error) => {
+        console.error("Error fetching data:", error);
+        });
+}, []);
+
+    return (
     <div>
-        <h1>Solar System Data</h1>
-        <ul>
-        {data.map((planet: any) => (
-            <li key={planet.id}>
-            <h2>{planet.englishName}</h2>
-            <img src={planet.image} alt={planet.englishName} />
-            </li>
-        ))}
-        </ul>
+        <h1>Random Celestial Body</h1>
+        {solarData && (
+        <div>
+            <h2>{solarData.englishName}</h2>
+            <img src={solarData.image} alt={solarData.englishName} />
+            <p>{solarData.description}</p>
+        </div>
+        )}
     </div>
     );
 };
 
-export default SolarSystemData;
+export default SolarSystemOpenData;
