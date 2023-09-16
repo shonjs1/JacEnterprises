52b0fa4aa9ae8bed50bb8ce9ff04cf5891f63eb1
owltober <lecanhhuan@gmail.com>
2023-09-15 23:09:43 -0400

    100% about page

--- client/vite-project/src/pages/AboutUs/Aboutus.tsx
+++ client/vite-project/src/pages/AboutUs/Aboutus.tsx
@@ -8,7 +8,7 @@ const AboutUsPage = () => {
 
                 <div className="row">
                     <div className="col">
-                        about
+                    <h3 className="font-style margin-bottom-2 margin-top-1"> We are a diverse and driven group of individuals, each on our unique journey towards excellence.</h3>
                     </div>
                 </div>
 
@@ -16,57 +16,77 @@ const AboutUsPage = () => {
                     <div className="col-sm-4 col-border">
                         
                         <img className="profilepic" src="/src/images/Alicia pic.png" alt="AliMar" width="60%" />
-                        <h4>Alicia Tejada</h4>
+                        <h4 className="margin-top-1 font-style">Alicia Tejada</h4>
                     </div>
-                    <div className="col-sm-8 col-border">
-                        <h5>An Mom Artist</h5>
-                        <p>
+                    <div className="col-sm-8 col-border ">
+                        <h5 className="margin-top-2 font-style">An Mom Artist</h5>
+                        <p className="aboutus-p">
                         I'm Alicia Tejada, an artist and mother. My passion for painting, especially portraits, has been a colorful journey of self-expression. I also create vibrant textile art. Inspired by my children, I explore new techniques and push creative boundaries. Explore my artwork here and let's connect for endless inspiration.
                         </p>
+                        <a className="block" href="https://github.com/Tejaalic" target="_blank" rel="noreferrer" title="View Alicia's Github">                            
+                            <svg className="logo" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                            </svg>                
+                        </a>
                     </div>
                 </div>
 
                 <div className="row">
-                    <div className="col-sm-4 col-border">
+                    <div className="col-sm-4 col-border ">
                         <img className="profilepic" src="https://ca.slack-edge.com/T14LST83D-U056ZDN68F9-7997599e3a9a-512" alt="Ji" width="60%" />
-                        <h4>Ji Hyuk Shon</h4>
+                        <h4 className="margin-top-1 font-style">Ji Hyuk Shon</h4>
                     </div>
-                    <div className="col-sm-8 col-border">
-                        <h5>Aspiring Software Developer</h5>
-                        <p>
+                    <div className="col-sm-8 col-border ">
+                        <h5 className="margin-top-2 font-style">Aspiring Software Developer</h5>
+                        <p className="aboutus-p">
                         Hello, I'm Ji Hyuk Shon.
 
                         I am a full stack software engineer and I want to be apart of a team that delivers top results, has a strong sense of purpose, and a desire to create value for our customers through innovation.
                         </p>
+                        <a className="block" href="https://github.com/shonjs1" target="_blank" rel="noreferrer" title="View Ji's Github">                            
+                            <svg className="logo" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                            </svg>                
+                        </a>
                     </div>
                 </div>
 
                 <div className="row">
-                    <div className="col-sm-4 col-border">
+                    <div className="col-sm-4 col-border ">
                         <img className="profilepic" src="/src/images/DCamron.jpg" alt="D'Camron" width="60%" />
-                        <h4>D'Camron Dunlap</h4>
+                        <h4 className="margin-top-1 font-style">D'Camron Dunlap</h4>
                     </div>
-                    <div className="col-sm-8 col-border">
-                        <h5>Aspiring Software Developer</h5>
-                        <p>
+                    <div className="col-sm-8 col-border ">
+                        <h5 className="margin-top-2 font-style">Aspiring Software Developer</h5>
+                        <p className="aboutus-p">
                             I am a aspiring Software Developer, who has taken his first step forward, by enrolling in the We Can Code It IT bootcamp, As well as participating in the Amazon Technical Academy Java training program.
                             I enjoy all things tech, because it's one of the few things left in the world, where not only the possibilities, but the learning potential is infinite.
                             A dream is to one day have the opportunity to push something to it's limits to see what humanity is capable of in the realm of technology.
                         </p>
+                        <a className="block" href="https://github.com/Numbah6" target="_blank" rel="noreferrer" title="View D'Camron's Github">                            
+                            <svg className="logo" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                            </svg>                
+                        </a>
                     </div>
                 </div>
 
-                <div className="row">
-                    <div className="col-sm-4 col-border">
+                <div className="row ">
+                    <div className="col-sm-4 col-border ">
                         <img className="profilepic" src="https://huanle25.github.io/assets/images/profile%20pic.jpg" alt="D'Camron" width="60%" />
-                        <h4>Huan Le</h4>
+                        <h4 className="margin-top-1 font-style">Huan Le</h4>
                     </div>
-                    <div className="col-sm-8 col-border">
-                        <h5>Aspiring Software Developer</h5>
-                        <p>
+                    <div className="col-sm-8 col-border ">
+                        <h5 className="margin-top-2 font-style">Aspiring Software Developer</h5>
+                        <p className="aboutus-p">
                         Greetings! I'm Huan, a passionate learner who went on board on a astonishing programming journey later in life.
                         While some may find age as a hindrance, I believe it has only fueled my determination to master the art of programming and embrace the world of technology.
                         </p>
+                        <a className="block" href="https://github.com/huanle25" target="_blank" rel="noreferrer" title="View Huan's Github">                            
+                            <svg className="logo" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                            </svg>                
+                        </a>
                     </div>
                 </div>
 
