29011250d9f7ad87f0ed68a9d38d667e8ab772b4
shonjs <jihyukshon@gmail.com>
2023-09-15 22:23:39 -0400

    Dictionary search words works

--- server/server.js
+++ server/server.js
@@ -1,51 +1,32 @@
 const express = require('express');
-const axios = require('axios'); // Import axios
-const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 5000;
+const port = 5000; // Port for the backend server
 
-app.use(cors()); // Enable CORS for all routes
+// Use middleware to parse JSON requests
+app.use(bodyParser.json());
 
+// Create an array to store searched words in memory
+const searchedWords = [];
 
-let history = [];
+// Define a route to save a searched word
+app.post('/api/saveWord', (req, res) => {
+  const { word } = req.body;
 
-async function getNasa(search_term) {
-  try {
-    const response = await axios.get("https://images-api.nasa.gov/search?q=" + search_term);
-    if (response.status !== 200) {
-      throw new Error('Failed to fetch data from NASA API');
-    }
-
-    const data = response.data.collection.items;
-    
-    if (data.length === 0) {
-      throw new Error('No items found in NASA API response');
-    }
-
-    const randomItem = data[Math.floor(Math.random() * data.length)];
-
-    return randomItem;
-  } catch (error) {
-    console.error('Error fetching data from NASA API:', error);
-    throw error;
+  if (!word) {
+    return res.status(400).json({ error: 'Word is required' });
   }
-}
 
-app.get("/", async (req, res) => {
-  try {
-    const image = await getNasa(req.query.q);
-    history.push(image.links[0].href);
-    res.status(200).json({
-      "message": "Object Saved!",
-      "history": history
-    });
-  } catch (error) {
-    res.status(500).json({
-      "message": "Error fetching data from NASA API"
-    });
-  }
+  searchedWords.push(word);
+
+  return res.status(200).json({ message: 'Word saved successfully' });
+});
+
+// Define a route to get the list of searched words
+app.get('/api/getSearchedWords', (req, res) => {
+  return res.status(200).json(searchedWords);
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
