5b806f94f3eb06a0ba4e050ee797bbdb812282a8
owltober <lecanhhuan@gmail.com>
2023-09-16 08:59:47 -0400

    change font style on home page

--- client/vite-project/src/pages/Home/Home.tsx
+++ client/vite-project/src/pages/Home/Home.tsx
@@ -7,9 +7,9 @@ const HomePage = () => {
                 <div className="row justify-content-md-center">
                     <div className="col-md-auto">
                             <img className="spaceship" src="/src/images/spaceship.png" alt="spaceship" width="30%" />
-                            <h1>Welcome, young explorers!</h1>
-              <p>Let's dive into the captivating world of planets by unveiling the Mystery Educator tab, where we'll uncover thrilling and fascinating facts about these celestial wonders! Join us on this thrilling adventure!</p>
-              <p>Click on the flying astronaut for a small treat!</p>
+                            <h1 className ="font-style">Welcome, young explorers!</h1>
+              <p className ="font-style font-small">Let's dive into the captivating world of planets by unveiling the Mystery Educator tab, where we'll uncover thrilling and fascinating facts about these celestial wonders! Join us on this thrilling adventure!</p>
+              <p className ="font-style font-small">Click on the flying astronaut for a small treat!</p>
           </div>
         </div>
       </div>
